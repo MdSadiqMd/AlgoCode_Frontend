1aabb81e3803b406bfdef8240196df20b56a8132
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-08-10 20:58:36 +0530

    Refactor Added Tabs and removed minimap

--- src/app/problems/[slug]/page.tsx
+++ src/app/problems/[slug]/page.tsx
@@ -8,8 +8,14 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Page: NextPage = () => {
+    const options = {
+        readOnly: false,
+        minimap: { enabled: false },
+    };
+
     return (
         <div className="w-screen h-screen">
             <ResizablePanelGroup
@@ -17,8 +23,17 @@ const Page: NextPage = () => {
                 className="h-full w-full rounded-lg border"
             >
                 <ResizablePanel defaultSize={50}>
-                    <div className="flex h-full items-center justify-center p-6">
-                        <span className="font-semibold">Header</span>
+                    <div className="h-full w-full p-6">
+                        <Tabs defaultValue="problem" className="w-[275px] h-full">
+                            <TabsList className="flex items-start justify-start">
+                                <TabsTrigger value="problem">Problem</TabsTrigger>
+                                <TabsTrigger value="editorial">Editorial</TabsTrigger>
+                                <TabsTrigger value="submissions">Submissions</TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="problem">Problem content here</TabsContent>
+                            <TabsContent value="editorial">Editorial content here</TabsContent>
+                            <TabsContent value="submissions">Submissions content here</TabsContent>
+                        </Tabs>
                     </div>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
@@ -32,6 +47,7 @@ const Page: NextPage = () => {
                                         width="100%"
                                         defaultLanguage="javascript"
                                         defaultValue="// some comment"
+                                        options={options}
                                     />
                                 </div>
                             </div>
