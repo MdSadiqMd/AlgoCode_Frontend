7d976c17a7efff8f08a1310fca9b09e7c3258846
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-08-10 18:39:52 +0530

    Refactor Added Resizable Panel

--- src/app/problems/[slug]/page.tsx
+++ src/app/problems/[slug]/page.tsx
@@ -1,10 +1,40 @@
 import type { NextPage } from "next";
-import { Button } from "@/components/ui/button";
+
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 const Page: NextPage = () => {
     return (
         <>
-            <Button>Problem</Button>
+            <ResizablePanelGroup
+                direction="horizontal"
+                className="min-h-[300px] max-w-md rounded-lg border"
+            >
+                <ResizablePanel defaultSize={50}>
+                    <div className="flex h-full items-center justify-center p-6">
+                        <span className="font-semibold">Header</span>
+                    </div>
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel>
+                    <ResizablePanelGroup direction="vertical" className="h-full">
+                        <ResizablePanel defaultSize={50}>
+                            <div className="flex h-full items-center justify-center p-6">
+                                <span className="font-semibold">Content</span>
+                            </div>
+                        </ResizablePanel>
+                        <ResizableHandle withHandle />
+                        <ResizablePanel defaultSize={50}>
+                            <div className="flex h-full items-center justify-center p-6">
+                                <span className="font-semibold">Content</span>
+                            </div>
+                        </ResizablePanel>
+                    </ResizablePanelGroup>
+                </ResizablePanel>
+            </ResizablePanelGroup>
         </>
     );
 };
