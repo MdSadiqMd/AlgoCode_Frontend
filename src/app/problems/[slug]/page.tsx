a99c1b19277532af9c44465835673cf0326a6865
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-08-10 15:56:05 +0530

    Refactor Added dynamic routes

---
+++ src/app/problems/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Button } from "@/components/ui/button";
+
+const Page: NextPage = () => {
+    return (
+        <>
+            <Button>Problem</Button>
+        </>
+    );
+};
+
+export default Page;
