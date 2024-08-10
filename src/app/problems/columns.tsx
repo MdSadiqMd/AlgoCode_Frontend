2f0fbd150b4153880d50ecaca9a341f26ba8e714
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-08-10 17:20:48 +0530

    Refactor Added Shadcn Table with tanstack query

---
+++ src/app/problems/columns.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+type Problem = {
+    status: 'Not Attempted' | 'Attempted' | 'Completed';
+    title: number;
+    acceptance: number;
+    difficulty: 'easy' | 'medium' | 'hard';
+};
+
+const columns: ColumnDef<Problem>[] = [
+    {
+        accessorKey: "status",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Status
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+    },
+    {
+        accessorKey: "title",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Title
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+    },
+    {
+        accessorKey: "acceptance",
+        header: "Acceptance",
+    },
+    {
+        accessorKey: "difficulty",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Difficulty
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
+    },
+];
+
+export default columns;
--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,10 +1,111 @@
 import type { NextPage } from "next";
-import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+
+import columns from "./columns";
+import DataTable from "./data-table";
 
 const Page: NextPage = () => {
+    const data = [
+        {
+            "status": "Not Attempted",
+            "title": 101,
+            "acceptance": 67.5,
+            "difficulty": "easy"
+        },
+        {
+            "status": "Attempted",
+            "title": 102,
+            "acceptance": 55.3,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Completed",
+            "title": 103,
+            "acceptance": 82.1,
+            "difficulty": "hard"
+        },
+        {
+            "status": "Not Attempted",
+            "title": 104,
+            "acceptance": 74.2,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Attempted",
+            "title": 105,
+            "acceptance": 49.8,
+            "difficulty": "easy"
+        },
+        {
+            "status": "Not Attempted",
+            "title": 101,
+            "acceptance": 67.5,
+            "difficulty": "easy"
+        },
+        {
+            "status": "Attempted",
+            "title": 102,
+            "acceptance": 55.3,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Completed",
+            "title": 103,
+            "acceptance": 82.1,
+            "difficulty": "hard"
+        },
+        {
+            "status": "Not Attempted",
+            "title": 104,
+            "acceptance": 74.2,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Attempted",
+            "title": 105,
+            "acceptance": 49.8,
+            "difficulty": "easy"
+        },
+        {
+            "status": "Not Attempted",
+            "title": 101,
+            "acceptance": 67.5,
+            "difficulty": "easy"
+        },
+        {
+            "status": "Attempted",
+            "title": 102,
+            "acceptance": 55.3,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Completed",
+            "title": 103,
+            "acceptance": 82.1,
+            "difficulty": "hard"
+        },
+        {
+            "status": "Not Attempted",
+            "title": 104,
+            "acceptance": 74.2,
+            "difficulty": "medium"
+        },
+        {
+            "status": "Attempted",
+            "title": 105,
+            "acceptance": 49.8,
+            "difficulty": "easy"
+        }
+    ];
+
     return (
         <>
-            <Button>Problems</Button>
+            <div className="container mx-auto py-10">
+                <DataTable
+                    columns={columns as ColumnDef<{ status: string; title: number; acceptance: number; difficulty: string; }>[]}
+                    data={data}
+                />
+            </div>
         </>
     );
 };
